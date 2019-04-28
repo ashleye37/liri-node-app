061d31b8ea373a4d3899b1957c3a415eecf96348
Ashley Evans <ashley_e@live.com>
2019-04-27 19:27:00 -0700

    Adding files with installed spotify and axios. And adding API key for Spotify

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.dd95d9ee44674fbc83daea9dd0bc4c3b,
+  secret: process.env.d7ca8955462f4fbb883c067332e5e3c1
 };
 
 
