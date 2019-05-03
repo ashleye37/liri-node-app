83fe4a1905597772a3afc64af2bcdd89a7a547b4
Ashley Evans <ashley_e@live.com>
2019-05-03 15:32:51 -0700

    Adding updated code for liri.app

--- keys.js
+++ keys.js
@@ -3,6 +3,6 @@ console.log('this is loaded');
 exports.spotify = {
   id: process.env.dd95d9ee44674fbc83daea9dd0bc4c3b,
   secret: process.env.d7ca8955462f4fbb883c067332e5e3c1
-};
+}
 
 
--- liri.js
+++ liri.js
@@ -1,32 +1,103 @@
+// Pulled in data from dotenv, axios, moment and keys.js.
 require("dotenv").config();
-var keys = require("./keys.js");
-var axios = require("axios");
-var spotify = new Spotify(keys.spotify);
-var command = process.argv[2];
-var input = process.argv[3];
 
+// Set up of Axios and FS for use in node.
+var axios = require("axios");
+var fs = require("fs");
+
+// Set up of Moment for use in node.
+var moment = require("moment");
+moment().format();
+
+// Global variables needed to run LIRI app.
+var command = process.argv[2];
+var input = process.argv.slice(3).join(" ");
+
+// Linking all the input commands to the written functions.
 if (command === "concert-this") {
-  axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then(
-    function(response) {
-      console.log(response.data.venue[0]);
-      // console.log(response.data.datetime);
-      // console.log(response.data.venue.name);
-      // console.log(response.data.venue.city);
-    }
-  );
-} else if (command === "spotify this song") {
-  spotify.search({ type: 'track', query: input }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-  console.log(data); 
-  });
+    findConcerts(input);
+} else if (command === "spotify-this-song") {
+    findSongs(input);
 } else if (command === "movie-this") {
-  axios.get("http://www.omdbapi.com/?apikey=trilogy&" + input).then(
-    function(response) {
-      console.log(response);
-    }
-  );
+    findMovies(input);
 } else if (command === "do-what-it-says") {
-  
-}
+    doWhatItSays(input);
+};
+
+// Function that finds concerts for the selected artist.
+function findConcerts(input) {
+    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+                var date = response.data[i].datetime;
+                console.log("------------Concert Info------------",
+                    "\nArtist: " + input,
+                    "\nVenue: " + response.data[i].venue.name,
+                    "\nLocation: " + response.data[i].venue.city,
+                    "\nDate: " + moment(date).format('L'),
+                    "\n-----------------------------------")
+            }
+        })
+    .catch(function(error){
+        console.log("There was an error when trying to find concerts- please try again.");
+    })
+};
+
+
+
+// Function that finds information on the movie that was used as the input or returns information on Mr. Nobody.
+function findMovies() {
+    if (!input) {
+        input = "Mr Nobody"
+    }
+    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + input)
+    .then(function (response) {
+                console.log("------------Movie Info------------", "\nTitle: " + response.data.Title,
+                    "\nYear: " + response.data.Year,
+                    "\nimdbRating: " + response.data.imdbRating,
+                    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value,
+                    "\nCountry: " + response.data.Country,
+                    "\nLanguage: " + response.data.Language,
+                    "\nPlot: " + response.data.Plot,
+                    "\nActors: " + response.data.Actors,
+                    "\n----------------------------------")
+        })
+    .catch(function(error) {
+        console.log("There was an error when trying to find the requested movie- please try again.");
+    })
+};
+
+// Function that finds song information on the song that was used as the input or returns information on The Sign by Ace of Base.
+function findSongs(input) {
+    // Set up of Spotify api information for use in node.
+    var keys = require("./keys.js");
+    var Spotify = require("node-spotify-api");
+    var spotify = new Spotify(keys.spotify);
+    // If no input is provided the song The Sign will be added for the input.
+    if (!input) {
+        input = "The Sign";
+    } 
+    spotify.search({ type: 'track', query: input, limit: 1})
+    .then(function(data) {
+        console.log("------------",
+                "\nArtist: " + data.tracks.items[0].album.artists[0].name,
+                "\nAlbum: " + data.tracks.items[0].album.name,
+                "\nSong: " + data.tracks.items[0].name,
+                "\nSong URL: " + data.tracks.items[0].external_urls.spotify,
+                "------------")
+    })
+    .catch(function(error) {
+        console.log("There was an error when trying to find the requested song- please try again.")
+    })
+};
+
+function doWhatItSays(input) {
+    fs.readFile("random.txt", "utf-8", function (error, data) {
+        if (error) {
+            console.log("There was an error when trying to find the requested movie- please try again.")
+        } else {
+            var dataArr = data.split(",");
+            findSongs(dataArr[0],dataArr[1]);
+        }
+    })
+};
