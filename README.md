37e645bacebacf0437aa8165b73faa312e4b2dcb
Ashley Evans <ashley_e@live.com>
2019-05-03 17:24:10 -0700

    Updating code for Spotify and adding the logging of information to log.txt

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # liri-node-app
 Creating repository for LIRI- a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+## Requirements
+- Make a node.js app (called liri.js) that depends on the command and input from the user to get data from BandsInTown, Spotify, or OMDb APIs. 
+- Integrate BandsInTown, Spotify and OMDb APIs using appropriate NPM modules.
+- Use API calls to write data to the console after the user enters needed command and input.
+
+## Technologies Used
+- Node.js
+- JavaScript
+- Spotify API
+- OMDb API
+- BandsInTown API
+- Axios
+- FS
+- Moment
+- Git
+- GitHub
+
+## Code Explanation / Demo
+- There are 4 main commands that a user can type that will call different fuctions.
+  1) < concert-this > < artist/band name > will return the following.
+    - ------------Concert Info------------
+    - Artist: 
+    - Venue: 
+    - Location:
+    - Date: 
+    - ------------------------------------
+  2) < spotify-this-song > < song name > will return the following and if no song is provided "Ace of Base - The Sign" is added as the input. value.
+    - ------------Song Info------------
+    - Artist:
+    - Album: 
+    - Song:
+    - Song URL: 
+    - ---------------------------------
+  3) < movie-this > < movie name > will return the following and if no movie is provided "Mr. Nobody" is added as the input value.
+    - ------------Movie Info------------
+    - Title:
+    - Year: 
+    - imdbRating:
+    - Rotten Tomatoes Rating:
+    - Country: 
+    - Language:
+    - Plot:
+    - Actors: 
+    - ----------------------------------
+  4) < do-what-it-says > utilizes FS to read the "random.txt" file to pull "I want it that way" and will return the following.
+    - ------------Song Info------------
+    - Artist:
+    - Album: 
+    - Song:
+    - Song URL: 
+    - ---------------------------------
+  5) After each command is run it will also append the information logged in the console to "log.txt".
+
--- liri.js
+++ liri.js
@@ -1,6 +1,11 @@
 // Pulled in data from dotenv, axios, moment and keys.js.
 require("dotenv").config();
 
+// Set up of Spotify api information for use in node.
+var keys = require("./keys");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
 // Set up of Axios and FS for use in node.
 var axios = require("axios");
 var fs = require("fs");
@@ -22,7 +27,9 @@ if (command === "concert-this") {
     findMovies(input);
 } else if (command === "do-what-it-says") {
     doWhatItSays(input);
-};
+} else if (!command) {
+    console.log("Invalid Command- Please type any of the following commands: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says");
+}
 
 // Function that finds concerts for the selected artist.
 function findConcerts(input) {
@@ -36,11 +43,17 @@ function findConcerts(input) {
                     "\nLocation: " + response.data[i].venue.city,
                     "\nDate: " + moment(date).format('L'),
                     "\n-----------------------------------")
+                fs.appendFileSync("log.txt", "------------Concert Info------------");
+                fs.appendFileSync("log.txt", "\nArtist: " + input,);
+                fs.appendFileSync("log.txt", "\nVenue: " + response.data[i].venue.name);
+                fs.appendFileSync("log.txt", "\nLocation: " + response.data[i].venue.city);
+                fs.appendFileSync("log.txt", "\nDate: " + moment(date).format('L'));
+                fs.appendFileSync("log.txt", "\n-----------------------------------");
             }
         })
-    .catch(function(error){
-        console.log("There was an error when trying to find concerts- please try again.");
-    })
+        .catch(function (error) {
+            console.log("There was an error when trying to find concerts- please try again.");
+        })
 };
 
 
@@ -51,53 +64,82 @@ function findMovies() {
         input = "Mr Nobody"
     }
     axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + input)
-    .then(function (response) {
-                console.log("------------Movie Info------------", "\nTitle: " + response.data.Title,
-                    "\nYear: " + response.data.Year,
-                    "\nimdbRating: " + response.data.imdbRating,
-                    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value,
-                    "\nCountry: " + response.data.Country,
-                    "\nLanguage: " + response.data.Language,
-                    "\nPlot: " + response.data.Plot,
-                    "\nActors: " + response.data.Actors,
-                    "\n----------------------------------")
+        .then(function (response) {
+            console.log("------------Movie Info------------", "\nTitle: " + response.data.Title,
+                "\nYear: " + response.data.Year,
+                "\nimdbRating: " + response.data.imdbRating,
+                "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value,
+                "\nCountry: " + response.data.Country,
+                "\nLanguage: " + response.data.Language,
+                "\nPlot: " + response.data.Plot,
+                "\nActors: " + response.data.Actors,
+                "\n----------------------------------");
+            fs.appendFileSync("log.txt", "------------Movie Info------------");
+            fs.appendFileSync("log.txt", "\nTitle: " + response.data.Title);
+            fs.appendFileSync("log.txt", "\nYear: " + response.data.Year);
+            fs.appendFileSync("log.txt", "\nimdbRating: " + response.data.imdbRating);
+            fs.appendFileSync("log.txt", "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            fs.appendFileSync("log.txt", "\nCountry: " + response.data.Country);
+            fs.appendFileSync("log.txt", "\nLanguage: " + response.data.Language);
+            fs.appendFileSync("log.txt", "\nPlot: " + response.data.Plot);
+            fs.appendFileSync("log.txt", "\nActors: " + response.data.Actors);
+            fs.appendFileSync("log.txt", "\n----------------------------------");
         })
-    .catch(function(error) {
-        console.log("There was an error when trying to find the requested movie- please try again.");
-    })
+        .catch(function (error) {
+            console.log("There was an error when trying to find the requested movie- please try again.");
+        })
 };
 
 // Function that finds song information on the song that was used as the input or returns information on The Sign by Ace of Base.
-function findSongs(input) {
-    // Set up of Spotify api information for use in node.
-    var keys = require("./keys.js");
-    var Spotify = require("node-spotify-api");
-    var spotify = new Spotify(keys.spotify);
+function findSongs() {
     // If no input is provided the song The Sign will be added for the input.
     if (!input) {
-        input = "The Sign";
-    } 
-    spotify.search({ type: 'track', query: input, limit: 1})
-    .then(function(data) {
-        console.log("------------",
+        input = "Ace of Base The Sign";
+    }
+    spotify.search({ type: 'track', query: input, limit: 1 })
+        .then(function (data) {
+            console.log("------------Song Info------------",
                 "\nArtist: " + data.tracks.items[0].album.artists[0].name,
                 "\nAlbum: " + data.tracks.items[0].album.name,
                 "\nSong: " + data.tracks.items[0].name,
                 "\nSong URL: " + data.tracks.items[0].external_urls.spotify,
-                "------------")
-    })
-    .catch(function(error) {
-        console.log("There was an error when trying to find the requested song- please try again.")
-    })
+                "\n---------------------------------");
+        fs.appendFileSync("log.txt", "------------Song Info------------");
+        fs.appendFileSync("log.txt", "\nArtist: " + data.tracks.items[0].album.artists[0].name);
+        fs.appendFileSync("log.txt", "\nAlbum: " + data.tracks.items[0].album.name);
+        fs.appendFileSync("log.txt", "\nSong: " + data.tracks.items[0].name);
+        fs.appendFileSync("log.txt", "\nSong URL: " + data.tracks.items[0].external_urls.spotify);
+        fs.appendFileSync("log.txt", "\n---------------------------------");
+        })
+        .catch(function (error) {
+            console.log("There was an error when trying to find the requested song- please try again.")
+        })
 };
 
 function doWhatItSays(input) {
     fs.readFile("random.txt", "utf-8", function (error, data) {
         if (error) {
-            console.log("There was an error when trying to find the requested movie- please try again.")
+            console.log("There was an error when trying to read the text file- please try again.")
         } else {
             var dataArr = data.split(",");
-            findSongs(dataArr[0],dataArr[1]);
+            spotify.search({ type: 'track', query: dataArr[1], limit: 1 })
+                .then(function (data) {
+                    console.log("------------Song Info------------",
+                        "\nArtist: " + data.tracks.items[0].album.artists[0].name,
+                        "\nAlbum: " + data.tracks.items[0].album.name,
+                        "\nSong: " + data.tracks.items[0].name,
+                        "\nSong URL: " + data.tracks.items[0].external_urls.spotify,
+                        "\n---------------------------------");
+                    fs.appendFileSync("log.txt", "------------Song Info------------");
+                    fs.appendFileSync("log.txt", "\nArtist: " + data.tracks.items[0].album.artists[0].name);
+                    fs.appendFileSync("log.txt", "\nAlbum: " + data.tracks.items[0].album.name);
+                    fs.appendFileSync("log.txt", "\nSong: " + data.tracks.items[0].name);
+                    fs.appendFileSync("log.txt", "\nSong URL: " + data.tracks.items[0].external_urls.spotify);
+                    fs.appendFileSync("log.txt", "\n---------------------------------");
+                })
+                .catch(function (error) {
+                    console.log("There was an error when trying to find the requested song- please try again.")
+                })
         }
     })
 };
